b5797067ff1a81b25e5e65c710a0176d12261c14
cnorfleet0 <cnorfleet0@gmail.com>
2019-11-15 00:08:03 -0800

    started adding top structure and began building cosine wave generator

--- Verilog/finalProject.sv
+++ Verilog/finalProject.sv
@@ -1,12 +1,33 @@
 module top(input  logic clk, reset,
-			  input  logic chipSelect, sck, sdi);
+			  input  logic chipSelect, sck, sdi
+			  output logic carrier);
+	// Erik Meike and Caleb Norfleet
+	// FPGA stuff for uPs final project
 	
-	logic[11:0] freq;   // frequency of note signal
-	logic[15:0] wave;   // note signal
-	logic[7:0]  volume; // volume of output.  Used with PWM of carrier signal at 125 kHz
-	logic[15:0] mixed;  // carrier signal
+	logic [11:0] freq;       // frequency of note signal
+	logic [7:0]  wave;       // note signal. signed number
+	logic [7:0]  volume;     // unsigned volume of output
+	logic [7:0]  currentVol; // volume only updated after every 2^8 clock cycles
+	logic [7:0]  amplitude;  // amplitude of wave after multiplying with volume
+	//logic        carrier;    // output signal.  PWM at 40MHz to achieve amplitude at 156.25 kHz
 	
-	spi s(chipSelect, sck, sdi, freq, volume);
+	// main modules
+	spi         s(chipSelect, sck, sdi, freq, volume);
+	waveGen    wg(clk, reset, wgEn, freq, wave);
+	volumeMult vm(wave, volume, amplitude);
+	
+	// control signals
+	logic[7:0] waveCounter;
+	always_ff @(posedge clk) begin
+		if(reset)  waveCounter <= 8'b0;
+		else begin
+			waveCounter <= waveCounter + 8'b1;
+			if(wgEn) currentVol <= volume;
+		end
+	end
+	
+	logic wgEn; // wave gen runs at 156.25 kHz = 40MHz / 256 (aka 2^8)
+	assign wgEn = (waveCounter == 8'b0);
 	
 endmodule
 
@@ -38,4 +59,25 @@ module spi(input  logic chipSelect,
 		end
 		else dataCount <= 5'b0;
 	end
+endmodule
+
+module waveGen(input  logic clk, reset, wgEn,
+					input  logic[11:0] freq,
+					output logic[7:0]  wave)
+	// Caleb Norfleet, cnorfleet@hmc.edu, 11/14/19
+	// generates sinusoid at frequency (outputs signed number)
+	
+	logic[11:0] tuneWord;
+	logic[15:0] phaseAcc;        // phase accumulator
+	logic[7:0]  LUTcos[65535:0]; // look up table
+	
+	always_ff @(posedge clk) begin
+		if(reset)     phaseAcc <= 16'b0;
+		else if(wgEn) phaseAcc <= phaseAcc + {4'b0,tuneWord};
+	end
+	
+	initial begin
+		$readmemb("LUTcos.txt", LUTcos);
+	end
+	
 endmodule
