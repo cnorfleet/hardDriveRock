7346b6589746b005ba47db68000c7e3fa88b4c97
cnorfleet0 <cnorfleet0@gmail.com>
2019-12-09 12:28:39 -0800

    updated sam code for using multiple tracks

--- ATSAM/finalProject.c
+++ ATSAM/finalProject.c
@@ -2,17 +2,18 @@
 // cnorfleet@hmc.edu, emeike@hmc.edu 15 November 2019
 //
 // Sends song notes and volumes to FPGA over SPI
+// Note: store song pitch in Hz, dur in ms
 
 #include <stdio.h>
 #include "SAM4S4B_libraries/SAM4S4B.h"
 #include "ArduboyTonesPitches.h"
-
 #define TONES_END -1
+
+// Song to play:
 #include "Songs/demo/demo.c"
+#define NUM_TRACKS 1
+const int* tracks[NUM_TRACKS] = { &(score[0]) };
 
-#define PLAYGENERATEDSONG 1
-
-#define SONGMODESWITCH  PIO_PB2
 #define CHIP_SELECT_PIN PIO_PB10 // PB10 -> P126
 #define CHIP_SELECT_PIN2 PIO_PA8
 // SPCK: PA14 -> P113
@@ -24,194 +25,22 @@
 #define CLK_ID    TC_CLK5_ID
 #define CLK_SPEED TC_CLK5_SPEED
 
-#if PLAYGENERATEDSONG == 0
+unsigned int idx[NUM_TRACKS];
+uint16_t currentTuneWord[NUM_TRACKS];
+uint8_t  currentVolume = 0b11111111;
+int remainingDur[NUM_TRACKS];
+int currentDur = 0;
+char bytes[NUM_TRACKS*3];
 
-// Pitch in Hz, duration in ms
-// Fur Elise
-const int song1[][2] = {
-{659,	125},
-{623,	125},
-{659,	125},
-{623,	125},
-{659,	125},
-{494,	125},
-{587,	125},
-{523,	125},
-{440,	250},
-{  0,	125},
-{262,	125},
-{330,	125},
-{440,	125},
-{494,	250},
-{  0,	125},
-{330,	125},
-{416,	125},
-{494,	125},
-{523,	250},
-{  0,	125},
-{330,	125},
-{659,	125},
-{623,	125},
-{659,	125},
-{623,	125},
-{659,	125},
-{494,	125},
-{587,	125},
-{523,	125},
-{440,	250},
-{  0,	125},
-{262,	125},
-{330,	125},
-{440,	125},
-{494,	250},
-{  0,	125},
-{330,	125},
-{523,	125},
-{494,	125},
-{440,	250},
-{  0,	125},
-{494,	125},
-{523,	125},
-{587,	125},
-{659,	375},
-{392,	125},
-{699,	125},
-{659,	125},
-{587,	375},
-{349,	125},
-{659,	125},
-{587,	125},
-{523,	375},
-{330,	125},
-{587,	125},
-{523,	125},
-{494,	250},
-{  0,	125},
-{330,	125},
-{659,	125},
-{  0,	250},
-{659,	125},
-{1319,125},
-{  0,	250},
-{623,	125},
-{659,	125},
-{  0,	250},
-{623,	125},
-{659,	125},
-{623,	125},
-{659,	125},
-{623,	125},
-{659,	125},
-{494,	125},
-{587,	125},
-{523,	125},
-{440,	250},
-{  0,	125},
-{262,	125},
-{330,	125},
-{440,	125},
-{494,	250},
-{  0,	125},
-{330,	125},
-{416,	125},
-{494,	125},
-{523,	250},
-{  0,	125},
-{330,	125},
-{659,	125},
-{623,	125},
-{659,	125},
-{623,	125},
-{659,	125},
-{494,	125},
-{587,	125},
-{523,	125},
-{440,	250},
-{  0,	125},
-{262,	125},
-{330,	125},
-{440,	125},
-{494,	250},
-{  0,	125},
-{330,	125},
-{523,	125},
-{494,	125},
-{440,	500},
-{  0,	  1}, // stop
-{ -1,	  0}};
-
-// Pitch in Hz, duration in ms
-// Hedwig's Theme
-const int song2[][2] = {
-{ 494, 125 }, // B5
-{ 659, 187 }, // E5
-{ 784, 63  }, // G5
-{ 740, 125 }, // F#5
-{ 659, 250 }, // E5
-{ 988, 125 }, // B6
-{ 880, 375 }, // A6
-{ 740, 375 }, // F#5
-{ 659, 187 }, // E5
-{ 784, 63  }, // G5
-{ 740, 125 }, // F#5
-{ 622, 250 }, // D#5
-{ 698, 125 }, // Fnat5
-{ 494, 625 }, // B5
-{ 494, 125 }, // B5
-{ 659, 187 }, // E5
-{ 784, 63  }, // G5
-{ 740, 125 }, // F#5
-{ 659, 250 }, // E5
-{ 988, 125 }, // B6
-{ 1175,250 }, // D6
-{ 1109,125 }, // C#6
-{ 1047,250 }, // Cnat6
-{ 831, 125 }, // Aflat6
-{ 1047,187 }, // C6
-{ 988, 63  }, // B6
-{ 932, 125 }, // Bflat6
-{ 466, 250 }, // Bflat5
-{ 784, 125 }, // G5
-{ 659, 625 }, // E5
-{ 784, 125 }, // G5
-{ 988, 250 }, // B6
-{ 784, 125 }, // G5
-{ 988, 250 }, // B6
-{ 784, 125 }, // G5
-{ 1047,250 }, // C6
-{ 988, 125 }, // B6
-{ 932, 250 }, // Bflat6
-{ 740, 125 }, // F#5
-{ 784, 187 }, // G5
-{ 1047,63  }, // C6
-{ 988, 125 }, // Bnat6
-{ 466, 250 }, // Bflat5
-{ 494, 125 }, // B5
-{ 988, 625 }, // Bnat6
-{ 784, 125 }, // G5
-{ 988, 250 }, // B6
-{ 784, 125 }, // G5
-{ 988, 250 }, // B6
-{ 784, 125 }, // G5
-{ 1175,250 }, // D6
-{ 1109,125 }, // Dflat6
-{ 1047,250 }, // C6
-{ 831, 125 }, // Aflat6
-{ 1047,187 }, // C6
-{ 988, 63  }, // B6
-{ 932, 125 }, // Bflat6
-{ 466, 250 }, // Bflat5
-{ 784, 125 }, // G5
-{ 659 ,625 }, // E5
-{  0,	  1  }, // stop
-{ -1,   0 }};
-
-// near middle C:      A    B    C    D    E    F    G
-const int notes[] = { 196, 220, 240, 262, 294, 312, 350 }; // Hz
-
-#endif
-
-void playNote(int pitch, int dur);
+uint16_t getTuneWord(int pitch);
+int getMinDur(void);
+void updateTrackArray(int track);
+void initTrackArrays(void);
+char isAllRests(void);
+char isStillPlaying(void);
+void progressNotes(int timePassed);
+void updateBytes(int track);
+void sendNotes(void);
 
 int main(void) {
 	// Initialize:
@@ -222,8 +51,8 @@ int main(void) {
   spiInit(MCK_FREQ/244000, 0, 1);
   // "clock divide" = master clock frequency / desired baud rate
   // the phase for the SPI clock is 0 and the polarity is 0
-	//tcInit();
-	//tcChannelInit(CH_ID, CLK_ID, TC_MODE_UP_RC);
+//	tcInit();
+//	tcChannelInit(CH_ID, CLK_ID, TC_MODE_UP_RC);
 	tcDelayInit();
 	pioPinMode(CHIP_SELECT_PIN, PIO_OUTPUT);
 	pioPinMode(CHIP_SELECT_PIN2, PIO_OUTPUT);
@@ -232,54 +61,141 @@ int main(void) {
 //	while(1)
 //		temp = adcRead(ADC_CH0);
 
-	// Read desired song mode:
-	int songMode = pioDigitalRead(SONGMODESWITCH);
-#if PLAYGENERATEDSONG
-	const int * notes =  score;
-	const double speedMult = 1;
-	const int pitchMult = 1;
-#else
-	const int * notes = songMode ? &(song1[0][0]) : &(song2[0][0]);
-	const int speedMult = songMode ? 1 : 3;
-	const int pitchMult = songMode ? 1 : 2;
-#endif
-
-	// Play song:
-	int i = 0;
-	while (notes[i*2] == 0) { i++; } // skip rests at start
-	while (notes[i*2] != -1) {       // stop at TONES_END
-		if((i != 0) && (notes[i*2] != 0) && (notes[i*2] == notes[(i-1)*2]))
-			playNote(0, 10);
-		playNote(notes[i*2] / pitchMult, notes[i*2+1] * speedMult);
-		i++;
+	// Get ready to play song:
+	tcDelay(100); // allow for FPGA to start up
+	initTrackArrays();
+	while(isAllRests()) {
+		progressNotes(getMinDur()); // skip rests at start
 	}
+	
+	// Play song
+	while (isStillPlaying()) {
+		tcDelay(currentDur);
+		progressNotes(currentDur);
+		sendNotes();
+		
+		//tcResetChannel(CH_ID);
+		//uint32_t noteEnd = dur * (CLK_SPEED / 1e3);
+		//tcSetRC_compare(CH_ID, noteEnd);
+		//while(tcCheckRC_compare(CH_ID)) { }
+	}
+	
+	// stop playing at end of song:
+	for(int i = 0; i < TONES_END; i++) {
+		currentTuneWord[i] = 0;
+		remainingDur[i] = -1;
+		updateBytes(i);
+	}
+	sendNotes();
 }
 
-void playNote(int pitch, int dur) {
-	// pitch in Hz, dur in ms
-	
-	//tcResetChannel(CH_ID);
-	//uint32_t noteEnd = dur * (CLK_SPEED / 1e3);
-	//tcSetRC_compare(CH_ID, noteEnd);
-	
+uint16_t getTuneWord(int pitch) {
 	// note: tuneWord of 1 corresponds to 2.384 Hz = ((40MHz)/2^8)/2^16
 	uint16_t tuneWord = pitch / 2.38418579;
-	char tune_word_byte_1 = tuneWord >> 8;
-	char tune_word_byte_2 = tuneWord;
-	char volume_byte = (pitch == 0) ? 0b00000000 : 0b11111111; // pitch 0 is rest
+	return tuneWord;
+}
+
+int getMinDur(void) {
+	int minDur = tracks[0][2*idx[0]+1];
+	for(int i = 1; i < NUM_TRACKS; i++) {
+		if(remainingDur[i] < minDur) {
+			minDur = remainingDur[i];
+		}
+	}
+	return minDur;
+}
+
+void updateTrackArray(int track) {
+	remainingDur[track] = tracks[track][2*idx[track]+1];
+	currentTuneWord[track] = getTuneWord(tracks[track][2*idx[track]]);
+}
+
+void initTrackArrays(void) {
+	for(int i = 0; i < NUM_TRACKS; i++) {
+		idx[i] = 0;
+		updateTrackArray(i);
+	}
+	currentDur = getMinDur();
+}
+
+char isAllRests(void) {
+	for(int i = 0; i < NUM_TRACKS; i++) {
+		if(currentTuneWord[i] != 0) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+char isStillPlaying(void) {
+	for(int i = 0; i < NUM_TRACKS; i++) {
+		if(remainingDur[i] != -1)
+			return 1;
+	}
+	return 0;
+}
+
+void progressNotes(int timePassed) {
+	// update tracks after timePassed (in ms)
+	for(int i = 0; i < NUM_TRACKS; i++) {
+		if(remainingDur[i] == -1) continue;
+		remainingDur[i] = remainingDur[i] - timePassed;
+		if(remainingDur[i] <= 0) { // continute to next note
+			int lastRemainingDur = remainingDur[i];
+			idx[i] = idx[i] + 1;
+			if(tracks[i][2*idx[i]] == -1) { // at the end of this track
+				currentTuneWord[i] = 0;
+				remainingDur[i] = -1;
+			} else {
+				updateTrackArray(i);
+				remainingDur[i] = remainingDur[i] + lastRemainingDur;
+				// ^ if we've gone too far, subtract from next
+			}
+			updateBytes(i);
+		}
+	}
+	currentDur = getMinDur();
+}
+
+void updateBytes(int track) {
+	uint8_t tune_word_byte_1 = currentTuneWord[track] >> 8;
+	uint8_t tune_word_byte_2 = currentTuneWord[track];
+	uint8_t volume_byte = (currentTuneWord[track] == 0)
+					? 0b00000000 : currentVolume; // pitch 0 is rest
 	
+	bytes[3*track]   = tune_word_byte_1;
+	bytes[3*track+1] = tune_word_byte_2;
+	bytes[3*track+2] = volume_byte;
+}
+
+void sendNotes(void) {
 	// assert chipSelect
-	// shift in frequency in two bytes
-	// shift in volume in one byte
+	// for each track:
+	//   shift in frequency in two bytes
+	//   shift in volume in one byte
 	// deassert chipSelect
 	pioDigitalWrite(CHIP_SELECT_PIN, 1);
 	pioDigitalWrite(CHIP_SELECT_PIN2, 1);
-	spiSendReceive(tune_word_byte_1);
-	spiSendReceive(tune_word_byte_2);
-	spiSendReceive(volume_byte);
+	for(int i = 0; i < NUM_TRACKS*3; i++) {
+		spiSendReceive(bytes[i]);
+	}
 	pioDigitalWrite(CHIP_SELECT_PIN, 0);
 	pioDigitalWrite(CHIP_SELECT_PIN2, 0);
-	
-	tcDelay(dur);
-	//while(tcCheckRC_compare(CH_ID)) { }
 }
+
+/*
+use a RC_compare on a different channel for each track.
+never reset channels if possible, use count up and start counting
+again from bottom.  Calculate RC compare value to handle wraparound.
+also have a separate index i for each track.
+in main while loop, check all the RC compare triggers and also check
+for uart webpage updates.  send html stuff over uart to esp.
+also add a variable to keep track of whether or not a song is currently
+playing and which song is playing
+add html commands for pausing and continuing playing and also dynamically
+setting volume.  maybe even let volume change mid-note in addition to
+at the end of notes
+hmmm.  maybe instead of using a lot of RC_compares and different channels,
+find minimum time to next note change on any track since we might just send
+every note over SPI every time any note is updated anyways
+*/
