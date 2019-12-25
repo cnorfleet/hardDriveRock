83128956c118bdfc911544216a387fc32f86bd5e
cnorfleet0 <cnorfleet0@gmail.com>
2019-12-25 01:32:31 -0500

    minor stuff from projects day

--- ATSAM/Songs/4channel/heySoulSister.c
+++ ATSAM/Songs/4channel/heySoulSister.c
@@ -1,5 +1,5 @@
 #include "heySoulSister/heySoulSisterCH0.c"
-#include "heySoulSister/heySoulSisterCH0.c"
-#include "heySoulSister/heySoulSisterCH0.c"
+#include "heySoulSister/heySoulSisterCH3.c"
+#include "heySoulSister/heySoulSisterCH6.c"
 
 #define score4 score1
